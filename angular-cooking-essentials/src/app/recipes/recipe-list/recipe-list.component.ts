79b457507ca29bc32ff61628e2e50b2e4fb7eb8e
mmohmohoo <mmohmohoo85@gmail.com>
2018-04-30 15:45:00 +0100

    Added demos

---
+++ angular-cooking-essentials/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
+    
+    new Recipe('A test recipe', 'This is simply a test', 'http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
